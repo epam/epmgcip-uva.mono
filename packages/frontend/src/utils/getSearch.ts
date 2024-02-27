fc86202e757453e18472a2bd8818a1c890b5fa20
Andrei Roh <Andrei_Roh@epam.com>
2024-02-27 09:10:20 +0100

    feat(EPMGCIPUVA-3): Update getSearch util

--- packages/frontend/src/utils/getSearch.ts
+++ packages/frontend/src/utils/getSearch.ts
@@ -3,11 +3,12 @@ import { IUser } from 'src/types';
 export const getSearch = (users: IUser[], value?: string) => {
   if (users.length === 0 || !value) return users;
 
+  const unsensitiveValue = value.toLowerCase();
+
   return users.filter(
     (user) =>
-      user.name.startsWith(value) ||
-      user.name.toLowerCase().startsWith(value) ||
-      user.telegramName.toLowerCase().startsWith(value) ||
-      user.telegramName.slice(1).toLowerCase().startsWith(value)
+      user.name.toLowerCase().startsWith(unsensitiveValue) ||
+      user.telegramName.toLowerCase().startsWith(unsensitiveValue) ||
+      user.telegramName.slice(1).toLowerCase().startsWith(unsensitiveValue)
   );
 };
