14022d1d73dd2299b05de6fa65e78460b4def33d
Ihor Osadchyi <ihor_osadchyi@epam.com>
2024-03-25 16:53:31 +0200

    feat: Add deploy functions gh action

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,13 +1,18 @@
-import express from 'express';
-import * as functions from 'firebase-functions';
+import express from "express";
+import * as functions from "firebase-functions";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
+});
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'ok'
-    })
-})
+app.post("/auth", (req, res) =>
+  res.status(200).json({
+    status: "auth endpoint",
+  })
+);
 
-exports.app = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app);
