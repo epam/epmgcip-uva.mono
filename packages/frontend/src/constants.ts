b0a698224f0eabdf52af2bc2be8edd54ba7fed1f
Nataliia Tolokontseva <nataliia_tolokontseva@epam.com>
2024-04-25 12:26:30 +0300

    [EPMGCIPUVA-214] fix: Fix mispell in user exists error

--- packages/frontend/src/constants.ts
+++ packages/frontend/src/constants.ts
@@ -1,12 +1,4 @@
-import {
-  EventStatus,
-  Gender,
-  ImageType,
-  Language,
-  Month,
-  UserRole,
-  UserStatus,
-} from './types';
+import { EventStatus, Gender, ImageType, Language, Month, UserRole, UserStatus } from './types';
 import translation from 'src/translations/Russian.json';
 
 export const ROOT_ROUTE = '/';
@@ -29,20 +21,19 @@ export const USER_STATUSES = [
 
 export const DEFAULT_NOTIFICATION = 'Default Notification Message';
 export const NOTIFICATIONS = (value?: string) => ({
-  USER_EXISTS: `Извините, пользователь c именем ${value} в Telegram уже существует. Пожалуйста, проверьте данные или обратитесь с администратору`,
+  USER_EXISTS: `Извините, пользователь c именем ${value} в Telegram уже существует. Пожалуйста, проверьте данные или обратитесь к администратору`,
   USER_CREATED: `Пользователь ${value} успешно создан`,
   USER_CREATION_ERROR: `Ошибка при создании пользователя ${value}`,
-  USER_DOES_NOT_EXIST: `Извините, пользователь c именем ${value} в Telegram не существует. Пожалуйста, проверьте данные или обратитесь с администратору`,
+  USER_DOES_NOT_EXIST: `Извините, пользователь c именем ${value} в Telegram не существует. Пожалуйста, проверьте данные или обратитесь к администратору`,
   USER_UPDATED: `Пользователь ${value} успешно изменен`,
   USER_UPDATE_ERROR: `Ошибка при изменении пользователя ${value}`,
   USER_DELETED: `Пользователь ${value} успешно удален`,
   USER_DELETE_ERROR: `Ошибка при удалении пользователя ${value}`,
   USER_DOES_NOT_HAVE_ACCESS: translation.userDoesNotHaveAccess,
-  IMAGE_TOO_BIG:
-    'Извините, загрузка изображения невозможна. Размер не должен превышать 10MB',
+  IMAGE_TOO_BIG: 'Извините, загрузка изображения невозможна. Размер не должен превышать 10MB',
   IMAGE_WRONG_TYPE:
     'Извините, загрузка изображения невозможна. Разрешены следующие форматы изображений: png, jpg, jpeg, webp',
-  EVENT_EXISTS: `Извините, событие c id ${value} в Telegram уже существует. Пожалуйста, проверьте данные или обратитесь с администратору`,
+  EVENT_EXISTS: `Извините, событие c id ${value} в Telegram уже существует. Пожалуйста, проверьте данные или обратитесь к администратору`,
   EVENT_CREATED: `Событие ${value} успешно создано`,
   EVENT_CREATION_ERROR: `Ошибка при создании события ${value}`,
 });
@@ -60,8 +51,7 @@ export const EMPTY_USER = {
   status: UserStatus.Inactive,
 };
 
-export const DEVELOPMENT_ENVIRONMENT_URL =
-  'https://epmgcip-uva-develop.web.app/';
+export const DEVELOPMENT_ENVIRONMENT_URL = 'https://epmgcip-uva-develop.web.app/';
 export const LOCAL_OR_TEST_ENVIRONMENTS = ['localhost', 'epmgcip-uva-develop'];
 
 export const VOLUNTEER_GENDER = [
@@ -82,12 +72,7 @@ export const EVENT_STATUS = [
   { name: translation.draft, value: EventStatus.Draft },
 ];
 
-export const IMAGE_TYPE = [
-  ImageType.Jpeg,
-  ImageType.Jpg,
-  ImageType.Png,
-  ImageType.Webp,
-];
+export const IMAGE_TYPE = [ImageType.Jpeg, ImageType.Jpg, ImageType.Png, ImageType.Webp];
 
 // export const MONTHS = {
 //   [Month.January]: translation.january,
