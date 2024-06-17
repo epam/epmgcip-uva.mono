9bee1305927f3dae2803ecdd9745a60a963de9d3
Nataliia Tolokontseva <natolok@outlook.com>
2024-06-17 23:42:08 +0300

    Implement pagination for events page

--- packages/frontend/src/utils/getEvents.ts
+++ packages/frontend/src/utils/getEvents.ts
@@ -1,17 +1,39 @@
-import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
+import { QueryFieldFilterConstraint, QueryLimitConstraint, QueryOrderByConstraint, QueryStartAtConstraint, collection, getDocs, limit, orderBy, query, startAfter, where } from 'firebase/firestore';
 import { FirebaseCollection } from 'src/constants';
 import { firebaseDb } from 'src/main';
 import { FilterEventStatuses, IEvent } from 'src/types';
 
-/** Get filtered events list 
- * todo: make it actually pagable 
-*/
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getEvents = async (status?: FilterEventStatuses, _page: number = 0): Promise<IEvent[]> => {
-  const queryForEvents = status !== 'all' ?
-    query(collection(firebaseDb, FirebaseCollection.Events), where('status', '==', status), orderBy('startDate', 'desc'))
-    : query(collection(firebaseDb, FirebaseCollection.Events), orderBy('startDate', 'desc'));
+type QueryArgs = QueryFieldFilterConstraint | QueryOrderByConstraint | QueryStartAtConstraint | QueryLimitConstraint;
 
-  const querySnapshot = await getDocs(queryForEvents);
-  return querySnapshot.docs.map(doc => doc.data() as IEvent);
+interface GetEventsResult {
+  readonly events: IEvent[];
+  readonly lastEventStartDate: IEvent['startDate'] | null;
+}
+
+/** Get filtered events list */
+export const getEvents = async (
+  status?: FilterEventStatuses,
+  lastElementStartDate?: IEvent['startDate'] | undefined | null,
+  pageSize = 10
+): Promise<GetEventsResult> => {
+  const queryArgs: QueryArgs[] = [orderBy('startDate', 'desc'), limit(pageSize)];
+  if (status && status !== 'all') {
+    queryArgs.push(where('status', '==', status));
+  }
+  if (lastElementStartDate) {
+    queryArgs.push(startAfter(lastElementStartDate));
+  }
+  const queryForEvents = query(collection(firebaseDb, FirebaseCollection.Events), ...queryArgs);
+  try {
+    const querySnapshot = await getDocs(queryForEvents);
+
+    const events = querySnapshot.docs.map(doc => doc.data() as IEvent);
+    return {
+      events,
+      lastEventStartDate: events.length ? events[events.length - 1].startDate : null,
+    };
+  } catch (e) {
+    console.error("Could not retrieve events data", e);
+    return { events: [], lastEventStartDate: null };
+  }
 };
