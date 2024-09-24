4a27882dbebb6133156f7b005ae7f2dfa6815d01
hgh06 <hghazaryan006@gmail.com>
2024-09-24 19:27:17 +0400

    refactor: change volunteer creation id

--- packages/frontend/src/utils/createVolunteer.ts
+++ packages/frontend/src/utils/createVolunteer.ts
@@ -3,15 +3,15 @@ import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 import { firebaseDb } from 'src/main';
 import { IVolunteer } from 'src/types';
 import { showNotification } from './showNotification';
-import { getVolunteer, getVolunteerDocId } from './getVolunteer';
+import { getVolunteer } from './getVolunteer';
 import { FirebaseCollection, NOTIFICATIONS, STORAGE_BUCKET, VOLUNTEERS_IMAGES_PATH } from 'src/constants';
 
 export const createVolunteer = async (newVolunteer: IVolunteer, image: File): Promise<boolean> => {
-  const volunteer = await getVolunteer(newVolunteer.telegramName);
+  const volunteer = await getVolunteer(newVolunteer.telegramId);
   const volunteersRef = collection(firebaseDb, FirebaseCollection.Volunteers);
 
   if (volunteer) {
-    showNotification(NOTIFICATIONS(newVolunteer.telegramName).VOLUNTEER_EXISTS, 6000);
+    showNotification(NOTIFICATIONS().VOLUNTEER_EXISTS, 6000);
     return false;
   }
 
@@ -20,21 +20,21 @@ export const createVolunteer = async (newVolunteer: IVolunteer, image: File): Pr
 
     const imageStorageRef = ref(
       storage,
-      `${STORAGE_BUCKET}/${VOLUNTEERS_IMAGES_PATH}/${getVolunteerDocId(newVolunteer.telegramName)}`
+      `${STORAGE_BUCKET}/${VOLUNTEERS_IMAGES_PATH}/${newVolunteer.telegramId}`
     );
 
     await uploadBytes(imageStorageRef, image);
     const downloadUrl = await getDownloadURL(imageStorageRef);
-    await setDoc(doc(volunteersRef, getVolunteerDocId(newVolunteer.telegramName)), {
+    await setDoc(doc(volunteersRef, newVolunteer.telegramId), {
       ...newVolunteer,
       imageURL: downloadUrl,
     });
 
-    showNotification(NOTIFICATIONS(newVolunteer.telegramName).VOLUNTEER_CREATED, 3000);
+    showNotification(NOTIFICATIONS().VOLUNTEER_CREATED, 3000);
 
     return true;
   } catch {
-    showNotification(NOTIFICATIONS(newVolunteer.telegramName).VOLUNTEER_CREATION_ERROR, 3000);
+    showNotification(NOTIFICATIONS().VOLUNTEER_CREATION_ERROR, 3000);
 
     return false;
   }
--- packages/frontend/src/utils/getVolunteer.ts
+++ packages/frontend/src/utils/getVolunteer.ts
@@ -1,24 +1,14 @@
 import { doc, getDoc } from 'firebase/firestore';
 import { firebaseDb } from 'src/main';
 import { IVolunteer } from 'src/types';
-import { FirebaseCollection, NOTIFICATIONS } from 'src/constants';
-import { showNotification } from './showNotification';
+import { FirebaseCollection } from 'src/constants';
 
-export const getVolunteerDocId = (telegramName: string) => telegramName.toLowerCase();
-
-/**
- * Get volunteer by lowercase telegram name.
- * if not found and telegramId is provided, try to find by telegramId and update telegramName.
- */
-export const getVolunteer = async (telegramName: string) => {
-  const docId = getVolunteerDocId(telegramName);
-
-  const docRef = doc(firebaseDb, FirebaseCollection.Volunteers, docId);
+export const getVolunteer = async (telegramId: string) => {
+  const docRef = doc(firebaseDb, FirebaseCollection.Volunteers, telegramId);
   const volunteer = await getDoc(docRef);
 
   if (volunteer.exists()) {
     return volunteer.data() as IVolunteer;
   }
-  showNotification(NOTIFICATIONS(telegramName).VOLUNTEER_DOES_NOT_EXIST, 3000)
   return undefined;
 };
